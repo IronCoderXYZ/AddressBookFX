9c0439e78cb545a693e62a9973305f4211352cfd
Mitch Hankins <mitchhankins92@gmail.com>
2017-10-11 16:25:50 -0600

    Added add person view

--- src/addressbookfx/AddressBookFX.java
+++ src/addressbookfx/AddressBookFX.java
@@ -2,13 +2,11 @@ package addressbookfx;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class AddressBookFX extends Application {
@@ -36,6 +34,19 @@ public class AddressBookFX extends Application {
         } catch(IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    private ObservableList<Person> contactList = FXCollections.observableArrayList();
+    public ObservableList<Person> getContactList() { return contactList; }
+    
+    public AddressBookFX() {
+        contactList.add(new Person("Mike", "Bowser", "524-111-2323", "Amsterdam", "3423FD"));
+        contactList.add(new Person("Anna", "Smith", "234-343-432", "Amsterdam", "3423FD"));
+        contactList.add(new Person("Joe", "Harbor", "122-111-3231", "Amsterdam", "3423FD"));
+        contactList.add(new Person("Marika", "Ruze", "323-222-2323", "Amsterdam", "3423FD"));
+        contactList.add(new Person("Sandra", "Malik", "456-444544", "Amsterdam", "3423FD"));
+        contactList.add(new Person("Jack", "Vale", "666-111-4566", "Amsterdam", "3423FD"));
+        contactList.add(new Person("Felix", "Corston", "444-111-4666", "Amsterdam", "3423FD"));
     }
     
     public static void main(String[] args) {
--- src/addressbookfx/MainController.java
+++ src/addressbookfx/MainController.java
@@ -1,16 +1,42 @@
 package addressbookfx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MainController {
+    @FXML TableView<Person> tableView;
+    @FXML TableColumn<Person, String> fNameCol;
+    @FXML TableColumn<Person, String> lNameCol;
+    @FXML Label fNameLabel, lNameLabel, phoneLabel, cityLabel, zipLabel;
+    
     private AddressBookFX addressBookFX;
+    
+    public void initialize() {
+        fNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        
+        tableView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldVal, newVal) -> showDetails(newVal));
+    }
     
     public void setMain(AddressBookFX addressBookFX) {
         this.addressBookFX = addressBookFX;
+        tableView.setItems(addressBookFX.getContactList());
+    }
+    
+    public void showDetails(Person person) {
+        fNameLabel.setText(person.getFirstName());
+        lNameLabel.setText(person.getLastName());
+        phoneLabel.setText(person.getPhone());
+        cityLabel.setText(person.getCity());
+        zipLabel.setText(person.getZIP());
     }
     
     @FXML
-    public void newPressed() {
+    public void addPressed() {
         
     }
     
